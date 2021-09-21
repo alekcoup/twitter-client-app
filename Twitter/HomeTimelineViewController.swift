45a2ab98be5961c69c15930d0c99369e8a3c0b84
alekcoup <coupetalek1@gmail.com>
2021-09-20 20:15:27 -0400

    Twitter Client

---
+++ Twitter/HomeTimelineViewController.swift
@@ -0,0 +1,153 @@
+//
+//  HomeTimelineViewController.swift
+//  Twitter
+//
+//  Created by Alek Coupet on 9/20/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTimelineViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tweetArray = [NSDictionary]()
+    var numberOfTweet: Int!
+    
+    let myRefreshControl = UIRefreshControl()
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.delegate = self;
+        self.tableView.dataSource = self;
+        
+        
+
+        // Do any additional setup after loadin
+
+        self.loadTweets()
+        
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+        }
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    @objc func loadTweets(){
+        
+        
+        numberOfTweet = 20
+        
+        
+        
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": numberOfTweet]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            
+            self.tweetArray.removeAll()
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            
+            self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+            
+        }, failure: { Error in
+            print("Could not retrieve tweets! oh no!")
+        })
+        
+        
+    }
+    
+    
+    
+    
+    func loadMoreTweets(){
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        numberOfTweet = numberOfTweet + 20
+        let myParams = ["count": numberOfTweet]
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            
+            self.tweetArray.removeAll()
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            
+            self.tableView.reloadData()
+        }, failure: { Error in
+            print("Could not retrieve tweets! oh no!")
+        })
+    }
+    
+    
+    
+     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath){
+        if indexPath.row + 1 == tweetArray.count{
+            loadMoreTweets()
+        }
+    }
+    
+    
+
+    
+    
+    
+    
+    
+    
+
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+        
+    }
+    
+    
+    
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+        
+    let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+    
+    
+    cell.userNameLabel.text = user["name"] as? String
+    cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
+    
+    let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+    let data = try? Data(contentsOf: imageUrl!)
+   
+    if let imageData = data {
+        cell.profileImageView.image = UIImage(data: imageData)
+    }
+        return cell
+    }
+    
+    
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.tweetArray.count
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
